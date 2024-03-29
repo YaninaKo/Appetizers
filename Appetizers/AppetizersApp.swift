a8b7f96084a573a85829ec8fa4ca8c466866caf2
YaninaKo <yanina.kovrakh@gmail.com>
2024-03-29 12:42:59 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yanina Kovrakh on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
